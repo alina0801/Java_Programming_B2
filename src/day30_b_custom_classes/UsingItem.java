e7393575eb043050cd29f1ad7d1eab90c407399f
alina0801 <adjz123@yahoo.com>
2023-11-09 19:02:31 -0500

    Loops with break and cont

--- src/day30_b_custom_classes/UsingItem.java
+++ src/day30_b_custom_classes/UsingItem.java
@@ -1,4 +1,21 @@
 package day30_b_custom_classes;
 
 public class UsingItem {
+    public static void main(String[] args) {
+        Item itemOne = new Item();
+        //object reference == actual objet
+        itemOne.name = "Apples";
+        itemOne.price = 5.9;
+
+        System.out.println(itemOne);
+
+
+        Item itemTwo = new Item();
+        itemTwo.name = "Water";
+        itemTwo.price = 1.99;
+        System.out.println(itemTwo);
+
+
+
+    }
 }
